4dc325b961010ec98b205350846bd9c1682ab83d
Amin Oruji <aminpy@aminpy.com>
2012-12-31 13:39:39 +0330

    fix xml namespace warning

---
+++ Phonebook-ejb/src/main/java/com/aminpy/phonebook/exception/person/RemovingRelationException.java
@@ -0,0 +1,13 @@
+package com.aminpy.phonebook.exception.person;
+
+public class RemovingRelationException extends Exception {
+	private static final long serialVersionUID = -6113826235231658999L;
+
+	public RemovingRelationException() {
+		super();
+	}
+
+	public RemovingRelationException(String message) {
+		super(message);
+	}
+}
